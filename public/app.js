ea01d19952f78301eabc0616ba416b4668036b89
bassmonkey620 <bassmonkey620@users.noreply.github.com>
2021-05-02 20:52:38 -0400

    feat: add get content functionality

--- public/app.js
+++ public/app.js
@@ -1,18 +1,36 @@
 const app = {
 
    templates: {
-      landing: $("#landing"),
+      landing: "#landing",
+      letter: "#letter",
+      letterPhotos: "#letter-photos",
+      video: "#video",
+      closing: "#closing",
+      gallery: "#gallery",
    },
 
-   init: function() {
-      console.log("hi");
-      app.templates.landing.hide();
-      console.log(pages.landing);
+   btns: {
+      next: ".btn-next",
+   },
+
+   forms: {
+      login: ".form-login",
+   },
+
+   init: function() {      
+      $.each(app.templates, (key, value) => $(value).hide());
+      app.landingPage();
+   },
+
+   landingPage: function() {
+      $(templates.landing).show();
+      $(forms.locgin).submit(() => {app.content()});
+   },
+
+   getContent: function(contentKey, getAll = false) {
+      $.get("/content", { key: contentKey, all: getAll })
+      .done(data => console.log(data));
    }
 }
 
-jQuery(
-   () => {
-      app.init();
-   }
-)
+$(() => app.init())
--- server.js
+++ server.js
@@ -1,9 +1,78 @@
 // const https = require("https");
-// const fs = require("fs");
+const path = require("path");
+const fs = require("fs");
 
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+app.use("/assets", express.static("assets"));
+
+const nedb = require("nedb");
+const contentParams = new nedb({ filename: "databases/content-params.db", autoload: true });
+const vidDb = new nedb({ filename: "databases/video-links.db", autoload: true });
+
+contentParams.ensureIndex({ fieldName: "key", unique: true }, err => console.log(err));
+vidDb.ensureIndex({ fieldName: "key", unique: true }, err => console.log(err));
+
+app.get(
+  "/content",
+  (req, res) => {
+    contentParams.find(
+      req.query, 
+      async (err, data) => {
+        if (err) throw err;
+
+        const params = data[0];
+
+        const returnData = {
+          text: `/assets/text/${params.key}.txt`,
+        }
+
+        switch(params.type) {
+          
+          case "letter":
+            res.send(returnData);
+            break;
+
+          case "letter-images":
+            const imgPath = `assets/images/${params.key}`;
+
+            fs.readdir(
+              path.resolve(__dirname, imgPath),
+              (err, files) => {
+                if (err) throw err;
+
+                const filePaths = [];
+
+                files.forEach(file => {
+                  filePaths.push(`${imgPath}/${file}`);
+                })
+
+                returnData.imgs = filePaths;
+
+                res.send(returnData);
+              });
+            break;
+
+          case "video":
+            vidDb.find(
+              req.query,
+              (err, data) => {
+                if (err) throw(err);
+                
+                returnData.vid = data[0].link;
+                
+                res.send(returnData);
+              });
+            break;
+
+          default:
+            break;
+        }
+
+    })
+  }
+)
 
 // const credentials = {
 //    key: fs.readFileSync("cert/privkey1.pem"),
